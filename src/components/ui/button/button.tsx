153c55690736208d14d121642e0daa40d1df61ef
vustinchdev <vustinchdev@gmail.com>
2024-03-07 08:54:10 +0300

    feat: add button with BackArrowIcon

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,20 +1,33 @@
 import { ComponentPropsWithoutRef, ElementType } from 'react'
 
+import { BackArrowIcon } from '@/assets/icons'
+
 import s from './button.module.scss'
 
 export type ButtonProps<T extends ElementType = 'button'> = {
   as?: T
+  back?: boolean
   fullWidth?: boolean
   variant?: 'primary' | 'secondary'
 } & ComponentPropsWithoutRef<T>
 
 export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
-  const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
+  const {
+    as: Component = 'button',
+    back,
+    children,
+    className,
+    fullWidth,
+    variant = 'primary',
+    ...rest
+  } = props
 
   return (
     <Component
       className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
       {...rest}
-    />
+    >
+      {back && <BackArrowIcon />} {children}
+    </Component>
   )
 }
