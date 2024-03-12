96733711250943e24af411d3694907984055d411
EvgeniyaYakunina <Yakoonina@gmail.com>
2024-03-12 13:04:13 +0300

    create slider component

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -1,3 +1,31 @@
-export const Slider=()=>{
-  return <div></div>
+import { ComponentPropsWithoutRef } from 'react'
+
+import * as RadixSlider from '@radix-ui/react-slider'
+
+import s from './slider.module.scss'
+
+export type SliderProps = {
+  ariaLabelMax?: string
+  ariaLabelMin?: string
+  label?: string
+} & ComponentPropsWithoutRef<typeof RadixSlider.Root>
+
+export const Slider = (props: SliderProps) => {
+  const { ariaLabelMax, ariaLabelMin, className, label, value, ...rest } = props
+
+  return (
+    <div>
+      <div className={s.container}>
+        <span className={s.display}>{value?.[0]}</span>
+        <RadixSlider.Root className={`${s.sliderRoot} ${className}`} value={value} {...rest}>
+          <RadixSlider.Track className={s.sliderTrack}>
+            <RadixSlider.Range className={s.sliderRange} />
+          </RadixSlider.Track>
+          <RadixSlider.Thumb aria-label={ariaLabelMin} className={s.sliderThumb} />
+          <RadixSlider.Thumb aria-label={ariaLabelMax} className={s.sliderThumb} />
+        </RadixSlider.Root>
+        <span className={s.display}> {value?.[1]} </span>
+      </div>
+    </div>
+  )
 }
