de523888c7d5995d34619b0bf890e705d4863430
EvgeniyaYakunina <Yakoonina@gmail.com>
2024-03-12 12:02:55 +0300

    add forwardRef & typing in Card component

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,13 +1,18 @@
-import { ComponentPropsWithoutRef, ElementType } from 'react'
+import { ComponentPropsWithoutRef, ElementType, forwardRef } from "react";
 
 import s from './card.module.scss'
+import clsx from "clsx";
+
+type InferType<T> = T extends ElementType<infer U> ? U : never
+
 
 export type CardProps<T extends ElementType = 'div'> = {
   as?: T
-} & ComponentPropsWithoutRef<'div'>
+} & ComponentPropsWithoutRef<T>
 
-export const Card = <T extends ElementType = 'div'>(props: CardProps<T>) => {
+export const Card = forwardRef(<T extends ElementType = 'div'>(props: CardProps<T>, ref: InferType<T>) => {
   const { as: Component = 'div', className, ...rest } = props
 
-  return <Component className={`${s.card} ${className}`} {...rest} />
+  return <Component className={clsx(s.card, className)} ref={ref} {...rest} />
 }
+)
