9da5d30c9e98b69b2fbe15b256f71f24118c2069
EvgeniyaYakunina <Yakoonina@gmail.com>
2024-03-12 12:20:01 +0300

    add forwardRef & typing in Typography component

--- src/components/ui/typography/typography.tsx
+++ src/components/ui/typography/typography.tsx
@@ -1,6 +1,10 @@
-import { ComponentPropsWithoutRef, ElementType } from 'react'
+import { ComponentPropsWithoutRef, ElementType, ForwardedRef, forwardRef } from 'react'
+
+import clsx from 'clsx'
 
 import s from './typography.module.scss'
+
+type InferType<T> = T extends ElementType<infer U> ? U : never
 
 export type TypographyProps<T extends ElementType = 'p'> = {
   as?: T
@@ -19,8 +23,10 @@ export type TypographyProps<T extends ElementType = 'p'> = {
     | 'subtitle2'
 } & ComponentPropsWithoutRef<T>
 
-export const Typography = <T extends ElementType = 'p'>(props: TypographyProps<T>) => {
-  const { as: Component = 'p', className, variant = 'body1', ...rest } = props
+export const Typography = forwardRef(
+  <T extends ElementType = 'p'>(props: TypographyProps<T>, ref: ForwardedRef<InferType<T>>) => {
+    const { as: Component = 'p', className, variant = 'body1', ...rest } = props
 
-  return <Component className={`${s.typography} ${s[variant]} ${className}`} {...rest} />
-}
+    return <Component className={clsx(s.typography, s[variant], className)} ref={ref} {...rest} />
+  }
+)
