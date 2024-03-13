5676520539344c1b074a6a4606bef6bb8b18b5a7
vustinchdev <vustinchdev@gmail.com>
2024-03-13 18:22:55 +0300

    refactor: change component checkbox

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,50 +1,29 @@
+import { ComponentPropsWithoutRef, ReactNode } from 'react'
+
 import { CheckboxIndicatorIcon } from '@/assets/icons'
 import * as CheckboxRadix from '@radix-ui/react-checkbox'
 
 import s from './checkbox.module.scss'
 
 export type CheckboxProps = {
-  checked?: boolean
-  className?: string
-  disabled?: boolean
-  id?: string
-  label?: string
-  onChange?: (checked: boolean) => void
-  required?: boolean
-}
+  label?: ReactNode
+} & ComponentPropsWithoutRef<typeof CheckboxRadix.Root>
 
-export const Checkbox = ({
-  checked,
-  className,
-  disabled,
-  id,
-  label,
-  onChange,
-  required,
-}: CheckboxProps) => {
+export const Checkbox = ({ className, disabled, id, label, ...rest }: CheckboxProps) => {
   return (
-    <form>
-      <div className={`${s.container} ${className}`}>
-        <div className={`${s.buttonWrapper} ${disabled ? s.disabled : ''}`}>
-          <CheckboxRadix.Root
-            checked={checked}
-            className={s.root}
-            disabled={disabled}
-            id={id}
-            onCheckedChange={onChange}
-            required={required}
-          >
-            <CheckboxRadix.Indicator className={s.indicator}>
-              <CheckboxIndicatorIcon />
-            </CheckboxRadix.Indicator>
-          </CheckboxRadix.Root>
-        </div>
-        {label && (
-          <label className={`${s.label} ${disabled ? s.disabledLabel : ''}`} htmlFor={id}>
-            {label}
-          </label>
-        )}
+    <div className={`${s.container} ${className}`}>
+      <div className={`${s.buttonWrapper} ${disabled ? s.disabled : ''}`}>
+        <CheckboxRadix.Root className={s.root} id={id} {...rest}>
+          <CheckboxRadix.Indicator className={s.indicator}>
+            <CheckboxIndicatorIcon />
+          </CheckboxRadix.Indicator>
+        </CheckboxRadix.Root>
       </div>
-    </form>
+      {label && (
+        <label className={`${s.label} ${disabled ? s.disabledLabel : ''}`} htmlFor={id}>
+          {label}
+        </label>
+      )}
+    </div>
   )
 }
