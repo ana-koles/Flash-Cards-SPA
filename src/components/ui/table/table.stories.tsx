bdf0dff51929a668de0d3024d94a2f1758ba50cf
Anastasija <anastasijakoles@gmail.com>
2024-03-13 19:16:34 +0400

    feat: add story

--- src/components/ui/table/table.stories.tsx
+++ src/components/ui/table/table.stories.tsx
@@ -1,6 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { TableWrapper } from './table'
+import {
+  TableBody,
+  TableBodyCell,
+  TableBodyRow,
+  TableHead,
+  TableHeadCell,
+  TableHeadRow,
+  TableWrapper,
+} from './table'
 
 const meta = {
   argTypes: {},
@@ -17,20 +25,35 @@ export const Table: Story = {
     children: <></>,
   },
   render: () => {
+    const tableColumnNames = ['Name', 'Cards', 'Last Updated', 'Created by', '']
+
+    const tableContent = [
+      { cards: 19, createdBy: 'Nick', icons: '*', name: 'JS', udpate: '22-01-2024' },
+      { cards: 9, createdBy: 'Bob', icons: '?', name: 'React', udpate: '01-01-2024' },
+      { cards: 20, createdBy: 'Lili', icons: '@', name: 'TS', udpate: '22-11-2023' },
+    ]
+
     return (
-      <div>
-        <ModalRoot onOpenChange={setIsOpen} open={isOpen}>
-          <ModalTrigger>
-            <button onClick={() => setIsOpen(true)}>Open</button>
-          </ModalTrigger>
-          <ModalContent modalTitle={'Modal Window'}>
-            <div>
-              <ChildrenComponent />
-              <Button onClick={() => setIsOpen(false)}>Cancel</Button>
-            </div>
-          </ModalContent>
-        </ModalRoot>
-      </div>
+      <TableWrapper>
+        <TableHead>
+          <TableHeadRow>
+            {tableColumnNames.map(name => (
+              <TableHeadCell key={name}>{name}</TableHeadCell>
+            ))}
+          </TableHeadRow>
+        </TableHead>
+        <TableBody>
+          {tableContent.map((card, index) => (
+            <TableBodyRow key={`${'c' + index}`}>
+              <TableBodyCell key={`${card.name + index}`}>{card.name}</TableBodyCell>
+              <TableBodyCell key={`${card.cards + index}`}>{card.cards}</TableBodyCell>
+              <TableBodyCell key={`${card.udpate + index}`}>{card.udpate}</TableBodyCell>
+              <TableBodyCell key={`${card.createdBy + index}`}>{card.createdBy}</TableBodyCell>
+              <TableBodyCell key={`${card.icons + index}`}>{card.icons}</TableBodyCell>
+            </TableBodyRow>
+          ))}
+        </TableBody>
+      </TableWrapper>
     )
   },
 }
