246b58537abca6ad3e49a484c5f7eedf2c022cb3
Anastasija <anastasijakoles@gmail.com>
2024-03-13 17:44:48 +0400

    feat: add main tables file and index

---
+++ src/components/ui/tables/tables.tsx
@@ -0,0 +1,31 @@
+export const Tables = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Cards</th>
+          <th>Last Updated</th>
+          <th>Created by</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Card 1</td>
+          <td>5</td>
+          <td>24-01-2024</td>
+          <td>Jane</td>
+          <td>+</td>
+        </tr>
+        <tr>
+          <td>Card 2</td>
+          <td>4</td>
+          <td>01-01-2024</td>
+          <td>Nicky</td>
+          <td>?</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
