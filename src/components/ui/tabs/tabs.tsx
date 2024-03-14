cf3b5d55dea5469a39e830b5f7938ae3f18c9dc9
Anastasija <anastasijakoles@gmail.com>
2024-03-14 22:17:27 +0400

    refactor: remove React. from tabs components, update style for tabsTrigger

--- src/components/ui/tabs/tabs.tsx
+++ src/components/ui/tabs/tabs.tsx
@@ -1,31 +1,28 @@
-import React, { ComponentPropsWithoutRef, ElementRef } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, ReactNode, forwardRef } from 'react'
 
 import * as Tabs from '@radix-ui/react-tabs'
 
 import s from './tabs.module.scss'
 
-type TabRootProps = {
-  children: React.ReactNode
-} & ComponentPropsWithoutRef<typeof Tabs.Root>
-
-export const TabRoot = React.forwardRef<ElementRef<typeof Tabs.Root>, TabRootProps>(
-  ({ children, defaultValue, ...restProps }: TabRootProps, ref) => {
-    return (
-      <Tabs.Root defaultValue={defaultValue} {...restProps} ref={ref}>
-        {children}
-      </Tabs.Root>
-    )
-  }
-)
+export const TabRoot = forwardRef<
+  ElementRef<typeof Tabs.Root>,
+  ComponentPropsWithoutRef<typeof Tabs.Root>
+>(({ children, defaultValue, ...restProps }, ref) => {
+  return (
+    <Tabs.Root defaultValue={defaultValue} {...restProps} ref={ref}>
+      {children}
+    </Tabs.Root>
+  )
+})
 
 TabRoot.displayName = 'TabRoot'
 
 type TabListProps = {
-  children: React.ReactNode
+  children: ReactNode
   orientation?: 'horizontal' | 'vertical'
 } & ComponentPropsWithoutRef<typeof Tabs.List>
 
-export const TabList = React.forwardRef<ElementRef<typeof Tabs.List>, TabListProps>(
+export const TabList = forwardRef<ElementRef<typeof Tabs.List>, TabListProps>(
   (props: TabListProps, ref) => {
     const { children, className, orientation = 'horizontal', ...restProps } = props
 
@@ -39,34 +36,28 @@ export const TabList = React.forwardRef<ElementRef<typeof Tabs.List>, TabListPro
 
 TabList.displayName = 'TabList'
 
-type TabTriggerProps = {
-  children: React.ReactNode
-} & ComponentPropsWithoutRef<typeof Tabs.Trigger>
-
-export const TabTrigger = React.forwardRef<ElementRef<typeof Tabs.Trigger>, TabTriggerProps>(
-  ({ children, className, value, ...restProps }: TabTriggerProps, ref) => {
-    return (
-      <Tabs.Trigger className={`${s.tabsTrigger}`} value={value} {...restProps} ref={ref}>
-        {children}
-      </Tabs.Trigger>
-    )
-  }
-)
+export const TabTrigger = forwardRef<
+  ElementRef<typeof Tabs.Trigger>,
+  ComponentPropsWithoutRef<typeof Tabs.Trigger>
+>(({ children, className, value, ...restProps }, ref) => {
+  return (
+    <Tabs.Trigger className={`${s.tabsTrigger}`} value={value} {...restProps} ref={ref}>
+      {children}
+    </Tabs.Trigger>
+  )
+})
 
 TabTrigger.displayName = 'TabTrigger'
 
-type TabContentProps = {
-  children: React.ReactNode
-} & ComponentPropsWithoutRef<typeof Tabs.Content>
-
-export const TabContent = React.forwardRef<ElementRef<typeof Tabs.Content>, TabContentProps>(
-  ({ children, ...restProps }: TabContentProps, ref) => {
-    return (
-      <Tabs.Content {...restProps} ref={ref}>
-        {children}
-      </Tabs.Content>
-    )
-  }
-)
+export const TabContent = forwardRef<
+  ElementRef<typeof Tabs.Content>,
+  ComponentPropsWithoutRef<typeof Tabs.Content>
+>(({ children, ...restProps }, ref) => {
+  return (
+    <Tabs.Content {...restProps} ref={ref}>
+      {children}
+    </Tabs.Content>
+  )
+})
 
 TabContent.displayName = 'TabContent'
