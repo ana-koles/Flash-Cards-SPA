7a5b764d5d88e09dff7b1a28bb8b3a9a1fda21ea
Anastasija <anastasijakoles@gmail.com>
2024-03-14 16:50:49 +0400

    feat: add arrow icon

--- src/components/ui/table/table.stories.tsx
+++ src/components/ui/table/table.stories.tsx
@@ -1,5 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import { useState } from 'react'
+
+import { ArrowAscIcon } from '@/assets/icons/arrowAsc'
+
+import s from './table.module.scss'
+
 import {
   TableBody,
   TableBodyCell,
@@ -9,6 +15,28 @@ import {
   TableHeadRow,
   TableWrapper,
 } from './table'
+
+type TableContentItem = {
+  cards: number
+  createdBy: string
+  icons: string
+  name: string
+  udpate: string
+}
+
+const tableColumnNames = [
+  { accessor: 'name', sortable: true, title: 'Name' },
+  { accessor: 'cards', sortable: true, title: 'Cards' },
+  { accessor: 'udpate', sortable: true, title: 'Last Updated' },
+  { accessor: 'createdBy', sortable: true, title: 'Created by' },
+  { accessor: 'icons', sortable: false, title: '' },
+]
+
+const tableContent: TableContentItem[] = [
+  { cards: 19, createdBy: 'Nick', icons: '*', name: 'JS', udpate: '22-01-2024' },
+  { cards: 9, createdBy: 'Bob', icons: '?', name: 'React', udpate: '01-01-2024' },
+  { cards: 20, createdBy: 'Lili', icons: '@', name: 'TS', udpate: '22-11-2023' },
+]
 
 const meta = {
   argTypes: {},
@@ -20,30 +48,112 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Table: Story = {
+export const TableWithDefaultData: Story = {
   args: {
     children: <></>,
   },
   render: () => {
-    const tableColumnNames = ['Name', 'Cards', 'Last Updated', 'Created by', '']
+    return (
+      <TableWrapper>
+        <TableHead>
+          <TableHeadRow>
+            <TableHeadCell>{tableColumnNames[0].title}</TableHeadCell>
+            <TableHeadCell>{tableColumnNames[1].title}</TableHeadCell>
+            <TableHeadCell>{tableColumnNames[2].title}</TableHeadCell>
+            <TableHeadCell>{tableColumnNames[3].title}</TableHeadCell>
+            <TableHeadCell>{tableColumnNames[4].title}</TableHeadCell>
+          </TableHeadRow>
+        </TableHead>
+        <TableBody>
+          <TableBodyRow>
+            <TableBodyCell>{tableContent[0].name}</TableBodyCell>
+            <TableBodyCell>{tableContent[0].cards}</TableBodyCell>
+            <TableBodyCell>{tableContent[0].udpate}</TableBodyCell>
+            <TableBodyCell>{tableContent[0].createdBy}</TableBodyCell>
+            <TableBodyCell>{tableContent[0].icons}</TableBodyCell>
+          </TableBodyRow>
+          <TableBodyRow>
+            <TableBodyCell>{tableContent[1].name}</TableBodyCell>
+            <TableBodyCell>{tableContent[1].cards}</TableBodyCell>
+            <TableBodyCell>{tableContent[1].udpate}</TableBodyCell>
+            <TableBodyCell>{tableContent[1].createdBy}</TableBodyCell>
+            <TableBodyCell>{tableContent[1].icons}</TableBodyCell>
+          </TableBodyRow>
+        </TableBody>
+      </TableWrapper>
+    )
+  },
+}
 
-    const tableContent = [
-      { cards: 19, createdBy: 'Nick', icons: '*', name: 'JS', udpate: '22-01-2024' },
-      { cards: 9, createdBy: 'Bob', icons: '?', name: 'React', udpate: '01-01-2024' },
-      { cards: 20, createdBy: 'Lili', icons: '@', name: 'TS', udpate: '22-11-2023' },
-    ]
+type SortOrder = 'asc' | 'desc'
+
+export const TableWithMapAndSorting: Story = {
+  args: {
+    children: <></>,
+  },
+  render: () => {
+    const [sortField, setSortField] = useState<string>('') //по чем сортируем
+    const [order, setOrder] = useState<SortOrder>('asc') // порядок сортировки
+    const [dataToDisplay, setDataToDisplay] = useState(tableContent)
+
+    const handleSorting = (sortField: string, sortOrder: SortOrder) => {
+      if (!sortField) {
+        return
+      }
+
+      const sorted = [...dataToDisplay].sort((a, b) => {
+        const orderDirection = sortOrder === 'asc' ? 1 : -1
+
+        return (
+          a[sortField].toString().localeCompare(b[sortField].toString(), {
+            numeric: true,
+          }) * orderDirection
+        )
+      })
+
+      setDataToDisplay(sorted)
+    }
+
+    const handleSortingChange = (accessor: string, sortable: boolean) => {
+      if (!sortable) {
+        return
+      }
+
+      let sortOrder: SortOrder = 'asc'
+
+      if (accessor === sortField) {
+        sortOrder = order === 'asc' ? 'desc' : 'asc'
+        setOrder(sortOrder)
+      } else {
+        setSortField(accessor)
+        setOrder(sortOrder)
+      }
+
+      handleSorting(accessor, sortOrder)
+    }
 
     return (
       <TableWrapper>
         <TableHead>
           <TableHeadRow>
-            {tableColumnNames.map(name => (
-              <TableHeadCell key={name}>{name}</TableHeadCell>
+            {tableColumnNames.map((column, index) => (
+              <TableHeadCell
+                key={`${column.accessor + index} `}
+                onClick={() => handleSortingChange(column.accessor, column.sortable)}
+                /*  onClick={column.sortable ? () => handleSortingChange(column.accessor) : () => {}} */
+              >
+                {column.title}
+                {column.accessor === sortField && (
+                  <span>
+                    {order === 'asc' ? <ArrowAscIcon /> : <ArrowAscIcon className={s.descIcon} />}
+                  </span>
+                )}
+              </TableHeadCell>
             ))}
           </TableHeadRow>
         </TableHead>
         <TableBody>
-          {tableContent.map((card, index) => (
+          {dataToDisplay.map((card, index) => (
             <TableBodyRow key={`${'c' + index}`}>
               <TableBodyCell key={`${card.name + index}`}>{card.name}</TableBodyCell>
               <TableBodyCell key={`${card.cards + index}`}>{card.cards}</TableBodyCell>
--- src/components/ui/table/table.tsx
+++ src/components/ui/table/table.tsx
@@ -1,36 +1,6 @@
 import { ComponentPropsWithoutRef, ElementRef, ReactNode, forwardRef } from 'react'
 
-export const Table = () => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Cards</th>
-          <th>Last Updated</th>
-          <th>Created by</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Card 1</td>
-          <td>5</td>
-          <td>24-01-2024</td>
-          <td>Jane</td>
-          <td>+</td>
-        </tr>
-        <tr>
-          <td>Card 2</td>
-          <td>4</td>
-          <td>01-01-2024</td>
-          <td>Nicky</td>
-          <td>?</td>
-        </tr>
-      </tbody>
-    </table>
-  )
-}
+import s from './table.module.scss'
 
 type TableWrapperProps = {
   children: ReactNode
@@ -39,7 +9,7 @@ type TableWrapperProps = {
 export const TableWrapper = forwardRef<HTMLTableElement, TableWrapperProps>(
   ({ children, className, ...restProps }: TableWrapperProps, ref) => {
     return (
-      <table className={''} {...restProps} ref={ref}>
+      <table className={s.tableWrapper} {...restProps} ref={ref}>
         {children}
       </table>
     )
@@ -55,7 +25,7 @@ type TableHeadProps = {
 export const TableHead = forwardRef<ElementRef<'thead'>, TableHeadProps>(
   ({ children, className, ...restProps }: TableHeadProps, ref) => {
     return (
-      <thead className={''} {...restProps} ref={ref}>
+      <thead className={s.tableHead} {...restProps} ref={ref}>
         {children}
       </thead>
     )
@@ -71,7 +41,7 @@ type TableHeadRowProps = {
 export const TableHeadRow = forwardRef<ElementRef<'tr'>, TableHeadRowProps>(
   ({ children, className, ...restProps }: TableHeadRowProps, ref) => {
     return (
-      <tr className={''} {...restProps} ref={ref}>
+      <tr className={`${s.tableRow} ${s.tableHeadRow}`} {...restProps} ref={ref}>
         {children}
       </tr>
     )
@@ -87,7 +57,7 @@ type TableHeadCellProps = {
 export const TableHeadCell = forwardRef<ElementRef<'th'>, TableHeadCellProps>(
   ({ children, className, ...restProps }: TableHeadCellProps, ref) => {
     return (
-      <th className={''} {...restProps} ref={ref}>
+      <th className={s.tableCell} {...restProps} ref={ref}>
         {children}
       </th>
     )
@@ -103,7 +73,7 @@ type TableBodyProps = {
 export const TableBody = forwardRef<ElementRef<'tbody'>, TableBodyProps>(
   ({ children, className, ...restProps }: TableBodyProps, ref) => {
     return (
-      <tbody className={''} {...restProps} ref={ref}>
+      <tbody className={s.tableBody} {...restProps} ref={ref}>
         {children}
       </tbody>
     )
@@ -119,7 +89,7 @@ type TableBodyRowProps = {
 export const TableBodyRow = forwardRef<ElementRef<'tr'>, TableBodyRowProps>(
   ({ children, className, ...restProps }: TableBodyRowProps, ref) => {
     return (
-      <tr className={''} {...restProps} ref={ref}>
+      <tr className={s.tableRow} {...restProps} ref={ref}>
         {children}
       </tr>
     )
@@ -135,7 +105,7 @@ type TableBodyCellProps = {
 export const TableBodyCell = forwardRef<ElementRef<'td'>, TableBodyCellProps>(
   ({ children, className, ...restProps }: TableBodyCellProps, ref) => {
     return (
-      <td className={''} {...restProps} ref={ref}>
+      <td className={s.tableCell} {...restProps} ref={ref}>
         {children}
       </td>
     )
