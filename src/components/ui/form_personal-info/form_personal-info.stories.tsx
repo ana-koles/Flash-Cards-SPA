afa2f5ff5b4fbc3f2196cc76c12fc425b883aa24
Anastasija <anastasijakoles@gmail.com>
2024-03-19 21:26:37 +0400

    feat: add avatar and styles

--- src/components/ui/form_personal-info/form_personal-info.stories.tsx
+++ src/components/ui/form_personal-info/form_personal-info.stories.tsx
@@ -11,4 +11,8 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Primary: Story = {}
+export const DefaultPersonalInfoForm: Story = {
+  args: {
+    nickName: 'Bruce Willis',
+  },
+}
--- src/components/ui/form_personal-info/form_personal-info.tsx
+++ src/components/ui/form_personal-info/form_personal-info.tsx
@@ -1,12 +1,20 @@
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
+import s from './from_personal-info.module.scss'
+
 import { Button } from '../button'
 import { Card } from '../card'
 import { FormInput } from '../input/form-input'
 import { Typography } from '../typography'
+import avatar from './avatar.png'
+
+type PersonalInfoFormProps = {
+  nickName: string
+}
 
 export const PersonalInfoSchema = z.object({
   nickName: z.string().trim(),
@@ -14,7 +22,7 @@ export const PersonalInfoSchema = z.object({
 
 export type PersonalInfoFormValues = z.infer<typeof PersonalInfoSchema>
 
-export const PersonalInfoForm = () => {
+export const PersonalInfoForm = ({ nickName }: PersonalInfoFormProps) => {
   const {
     control,
     formState: { errors },
@@ -23,22 +31,55 @@ export const PersonalInfoForm = () => {
     resolver: zodResolver(PersonalInfoSchema),
   })
 
+  const [editMode, setEditMode] = useState<boolean>(false)
+
   const onSubmit = (data: PersonalInfoFormValues) => {
     console.log(data)
   }
 
+  const handleSaveChanges = () => {
+    setEditMode(false)
+  }
+
+  const handleSetEditMode = () => {
+    setEditMode(true)
+  }
+
   return (
-    <Card>
-      <Typography variant={'h1'}>Personal Information</Typography>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <FormInput
-          control={control}
-          errorMessage={errors?.nickName?.message}
-          label={'Nickname'}
-          name={'nickName'}
-        />
-        <Button>Save Changes</Button>
-      </form>
+    <Card className={s.container}>
+      <div className={s.formWrapper}>
+        <div className={s.avatarSection}>
+          <Typography className={s.header} variant={'h1'}>
+            Personal Information
+          </Typography>
+          <img alt={'avatar'} className={s.avatar} src={avatar} />
+        </div>
+
+        {editMode ? (
+          <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+            <FormInput
+              control={control}
+              errorMessage={errors?.nickName?.message}
+              label={'Nickname'}
+              name={'nickName'}
+              placeholder={nickName}
+            />
+            <Button fullWidth onClick={handleSaveChanges}>
+              Save Changes
+            </Button>
+          </form>
+        ) : (
+          <div className={s.personalDataWrapper}>
+            <Typography className={s.nickName} onDoubleClick={handleSetEditMode} variant={'h2'}>
+              {nickName}
+            </Typography>
+            <Typography variant={'body2'}>j&johnson@gmail.com</Typography>
+            <Button back variant={'secondary'}>
+              Logout
+            </Button>
+          </div>
+        )}
+      </div>
     </Card>
   )
 }
