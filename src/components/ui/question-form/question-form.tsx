5c17848176d0952ff3bb518592ee873128c463b0
Anastasija <anastasijakoles@gmail.com>
2024-03-19 22:25:19 +0400

    feat: add question and answer sections to question-form component

---
+++ src/components/ui/question-form/question-form.tsx
@@ -0,0 +1,59 @@
+import { useForm } from 'react-hook-form'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+import s from './question-form.module.scss'
+
+import { Button } from '../button'
+import { Typography } from '../typography'
+
+type QuestionFormProps = {
+  deckName: string
+}
+
+const answerSchema = z.object({
+  answer: z.boolean(),
+})
+
+type FormValues = z.infer<typeof answerSchema>
+
+export const QuestionForm = ({ deckName }: QuestionFormProps) => {
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormValues>({
+    resolver: zodResolver(answerSchema),
+  })
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data)
+  }
+
+  return (
+    <div className={s.container}>
+      <Typography variant={'h1'}>Learn “{deckName}”</Typography>
+      <div className={s.questionSectionWrapper}>
+        <div className={s.questionTitle}>
+          <Typography as={'span'} variant={'subtitle1'}>
+            Question:
+          </Typography>
+          <Typography as={'span'} variant={'body1'}>
+            How &quotThis&quot works in JavaScript?
+          </Typography>
+        </div>
+        <div className={s.questionBody}></div>
+      </div>
+      <Typography variant={'body2'}>Количество попыток ответов на вопрос: 10</Typography>
+      <div className={s.answerSectionWrapper}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Typography as={'label'} variant={'subtitle2'}>Rate yourself:</Typography>
+          <Button fullWidth type={'submit'}>
+            Next Question
+          </Button>
+        </form>
+      </div>
+    </div>
+  )
+}
