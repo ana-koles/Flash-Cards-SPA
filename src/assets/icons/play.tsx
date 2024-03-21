c4ce542106453b0c15dd5e1d730815db06b88143
EvgeniyaYakunina <Yakoonina@gmail.com>
2024-03-21 20:21:31 +0300

    create decksTable directory & component

--- src/assets/icons/play.tsx
+++ src/assets/icons/play.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export const Play = () => (
   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g clipPath="url(#clip0_5661_2108)">
