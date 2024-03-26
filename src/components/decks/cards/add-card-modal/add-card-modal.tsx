ff100cec36b21d1f41580101c6b72ae0663878a7
Anastasija <anastasijakoles@gmail.com>
2024-03-26 22:03:48 +0400

    feat: add component with add card modal window

---
+++ src/components/decks/cards/add-card-modal/add-card-modal.tsx
@@ -0,0 +1,89 @@
+import { ReactNode, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+
+import defaultCardImg from '@/assets/defaultCardImg.png'
+import { Button } from '@/components/ui/button'
+import { FormInput } from '@/components/ui/input/form-input'
+import { ModalContent, ModalRoot } from '@/components/ui/modal'
+import { Typography } from '@/components/ui/typography'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+import s from './add-card-modal.module.scss'
+
+type AddCardModalProps = {
+  children: ReactNode
+  handleDataConfirm: (data: FormValues) => void
+  onOpenChange: (open: boolean) => void
+  open: boolean
+}
+
+const cardScheme = z.object({
+  answer: z.string().trim(),
+  question: z.string().trim(),
+})
+
+type FormValues = z.infer<typeof cardScheme>
+
+export const AddCardModal = ({
+  handleDataConfirm,
+  onOpenChange,
+  open,
+  ...restProps
+}: AddCardModalProps) => {
+  const { control, handleSubmit, reset } = useForm<FormValues>({
+    resolver: zodResolver(cardScheme),
+  })
+
+  const onSubmit = (data: FormValues) => {
+    handleDataConfirm(data)
+    onOpenChange(false)
+    reset()
+  }
+
+  const handleCancel = () => {
+    onOpenChange(false)
+    reset()
+  }
+
+  const classNames = {
+    title: s.title,
+  }
+
+  return (
+    <ModalRoot {...restProps} onOpenChange={onOpenChange} open={open}>
+      <ModalContent modalTitle={'Add New Card'}>
+        <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+          <div className={s.questionSection}>
+            <Typography as={'span'} className={classNames.title} variant={'subtitle2'}>
+              Question:
+            </Typography>
+            <FormInput control={control} label={'Question'} name={'question'} />
+            <input type={'file'} />
+            <Button fullWidth>
+              {/* <img alt={'card img'} src={defaultCardImg} /> */}
+              Change Image
+            </Button>
+          </div>
+          <div className={s.answerSection}>
+            <Typography as={'span'} className={classNames.title} variant={'subtitle2'}>
+              Answer:
+            </Typography>
+            <FormInput control={control} label={'Answer'} name={'answer'} />
+            <input type={'file'} />
+            <Button fullWidth>
+              {/* <img alt={'card img'} src={defaultCardImg} /> */}
+              Change Image
+            </Button>
+          </div>
+          <div className={s.buttonWrapper}>
+            <Button onClick={handleCancel} variant={'secondary'}>
+              Cancel
+            </Button>
+            <Button type={'submit'}>Add New Card</Button>
+          </div>
+        </form>
+      </ModalContent>
+    </ModalRoot>
+  )
+}
