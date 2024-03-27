ca59eb51c509df19b49a98757de924f42e090728
Anastasija <anastasijakoles@gmail.com>
2024-03-27 18:13:13 +0400

    refactor: add classNames with classes

---
+++ src/components/decks/delete-deck-modal/delete-deck-modal.stories.tsx
@@ -0,0 +1,53 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { useState } from 'react'
+
+import { Button } from '@/components/ui/button'
+
+import { DeleteDeckModule } from './delete-deck-modal'
+
+const meta = {
+  argTypes: {},
+  component: DeleteDeckModule,
+  tags: ['autodocs'],
+  title: 'Decks/ Delete Deck Modal',
+} satisfies Meta<typeof DeleteDeckModule>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+const deck = { id: '1', name: 'My Deck' }
+
+export const DeleteCardModalWindow: Story = {
+  args: {
+    children: <></>,
+    deck: deck,
+    handleDeckDelete: () => {},
+    onOpenChange: () => {},
+    open: true,
+  },
+  render: args => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleOpenChange = () => {
+      setIsOpen(true)
+    }
+
+    const handleDeckDelete = () => {
+      setIsOpen(true)
+    }
+
+    return (
+      <>
+        <Button onClick={handleOpenChange}>Click to open Modal Window</Button>
+        <DeleteDeckModule
+          {...args}
+          deck={deck}
+          handleDeckDelete={handleDeckDelete}
+          onOpenChange={setIsOpen}
+          open={isOpen}
+        ></DeleteDeckModule>
+      </>
+    )
+  },
+}
