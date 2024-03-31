364d7d269e70db0a32beb65016ac8f27459fe4a1
vustinchdev <vustinchdev@gmail.com>
2024-03-31 19:36:11 +0300

    feat: add styles for grade

--- src/components/ui/tables/grade/grade.tsx
+++ src/components/ui/tables/grade/grade.tsx
@@ -1,6 +1,8 @@
 import { StarIcon } from '@/assets/icons/star'
 import { StarOutlineIcon } from '@/assets/icons/star-outline'
 
+import s from './grade.module.scss'
+
 type Props = {
   maxGrade: number
   onClick: (value: number) => void
@@ -10,8 +12,12 @@ type Props = {
 export const Grade = ({ maxGrade, onClick, value }: Props) => {
   const stars = Array.from({ length: maxGrade }, (_, i) => i + 1)
 
+  const classNames = {
+    grade: s.grade,
+  }
+
   return (
-    <div>
+    <div className={classNames.grade}>
       {stars.map(star => (
         <Star key={star} onClick={onClick} selected={value >= star} value={star} />
       ))}
@@ -28,5 +34,13 @@ type StarProps = {
 const Star = ({ onClick, selected, value }: StarProps) => {
   const handleClick = () => onClick(value)
 
-  return <span onClick={handleClick}>{selected ? <StarIcon /> : <StarOutlineIcon />}</span>
+  const classNames = {
+    star: s.star,
+  }
+
+  return (
+    <span className={classNames.star} onClick={handleClick}>
+      {selected ? <StarIcon /> : <StarOutlineIcon />}
+    </span>
+  )
 }
