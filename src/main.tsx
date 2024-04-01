f1405407b856438e0780893662e416c43a1b7d9d
Anastasija <anastasijakoles@gmail.com>
2024-04-01 14:41:47 +0400

    fix: remove unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { StrictMode } from 'react'
-import { Provider } from 'react-redux'
 
 import { createRoot } from 'react-dom/client'
 
@@ -8,7 +7,6 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 
 import { App } from './App'
-import { store } from './services/store'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
