91be5c04a6c7970dcfb863c998ecc295dd68d95b
Anastasija <anastasijakoles@gmail.com>
2024-04-03 22:40:40 +0400

    refactor: udpate header

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -8,22 +8,21 @@ import s from './header.module.scss'
 
 export type HeaderProps = {
   isAuth: boolean
-  isLogedIn: boolean
   logout: () => void
   userData?: UserData
 }
 
-export const Header = ({ isLogedIn, ...restProps }: HeaderProps) => {
+export const Header = ({ isAuth, ...restProps }: HeaderProps) => {
   return (
     <div className={s.headerWrapper}>
       <div className={s.headerContent}>
         <img alt={'logo'} className={s.logo} src={logo} />
-        {isLogedIn ? (
+        {isAuth ? (
           <div className={s.menuWrapper}>
             <UserDropdown {...restProps} />
           </div>
         ) : (
-          <Button as={Link} to={'/login'} variant={'secondary'}>
+          <Button as={Link} className={s.loginBtn} to={'/login'} variant={'secondary'}>
             Sign in
           </Button>
         )}
