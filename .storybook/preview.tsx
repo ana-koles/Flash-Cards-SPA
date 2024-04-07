29c19c18b7f1d15e42d0422ac70f3e0316bab4a9
Anastasija <anastasijakoles@gmail.com>
2024-04-07 12:18:50 +0400

    fix: update story to display signin and signout

---
+++ .storybook/preview.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import type { Preview } from '@storybook/react'
+import '../src/styles/index.scss'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import { MemoryRouter } from 'react-router-dom'
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    Story => {
+      return (
+        <MemoryRouter>
+          <Story />
+        </MemoryRouter>
+      )
+    },
+  ],
+}
+
+export default preview
