ce3360d4451b7abccd4bd92b6c122a6019ef5994
Anastasija <anastasijakoles@gmail.com>
2024-04-10 19:55:26 +0400

    fix: fix stories

--- src/components/auth/forgot-password-form/forgot-password-form.stories.ts
+++ src/components/auth/forgot-password-form/forgot-password-form.stories.ts
@@ -14,6 +14,6 @@ type Story = StoryObj<typeof meta>
 
 export const ForgotPasswordFormDefault: Story = {
   args: {
-    onSubmit: data => console.log(data),
+    handlePasswordRecover: data => console.log(data),
   },
 }
