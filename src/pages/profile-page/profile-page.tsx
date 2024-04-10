3cb8150d271413f4246e330075e07b8be02fb20f
Anastasija <anastasijakoles@gmail.com>
2024-04-10 18:20:32 +0400

    refactor: put navigate and logout from persona-info-from to profile-page

--- src/pages/profile-page/profile-page.tsx
+++ src/pages/profile-page/profile-page.tsx
@@ -1,21 +1,32 @@
+import { useNavigate } from 'react-router-dom'
+
 import { PersonalInfoForm } from '@/components/ui/form_personal-info'
 import { useMeQuery, useUpdateUserDataMutation } from '@/services/auth'
+import { UpdateUserDataArgs } from '@/services/auth/auth.types'
 
 import s from './profile-page.module.scss'
 
 export const ProfilePage = () => {
   const { data } = useMeQuery()
   const [updateUserData] = useUpdateUserDataMutation()
+  const navigate = useNavigate()
 
-  console.log(data)
+  const handleUserUpdateData = (newUserData: UpdateUserDataArgs) => {
+    updateUserData(newUserData)
+  }
+
+  const handleLogout = () => {
+    navigate('/login')
+  }
 
   return (
     <div className={s.modalWrapper}>
       <PersonalInfoForm
         avatar={data?.avatar}
         email={data?.email}
+        handleLogout={handleLogout}
+        handleUserUpdateData={handleUserUpdateData}
         name={data?.name}
-        updateUserData={updateUserData}
       />
     </div>
   )
