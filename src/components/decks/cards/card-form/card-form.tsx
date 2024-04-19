377f33062b7070baa23955b8bcd10f258436a829
EvgeniyaYakunina <Yakoonina@gmail.com>
2024-04-19 15:08:58 +0300

    fix imports

--- src/components/decks/cards/card-form/card-form.tsx
+++ src/components/decks/cards/card-form/card-form.tsx
@@ -1,13 +1,13 @@
 import { useForm } from 'react-hook-form'
 
 import {
+  Button,
+  FormInput,
+  ImageUploader,
+  Typography,
   cardAddScheme,
   cardEditScheme,
-} from '@/components/decks/cards/card-validation/card-validation'
-import { ImageUploader } from '@/components/decks/cards/image-uploader/image-uploader'
-import { Button } from '@/components/ui/button'
-import { FormInput } from '@/components/ui/input/form-input'
-import { Typography } from '@/components/ui/typography'
+} from '@/components'
 import { CardResponse } from '@/services'
 import { zodResolver } from '@hookform/resolvers/zod'
 import z from 'zod'
