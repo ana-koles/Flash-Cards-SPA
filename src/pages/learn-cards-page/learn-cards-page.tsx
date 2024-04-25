e24e0a85c80130abd9a50ec75c8589d42b6fa185
vustinchdev <vustinchdev@gmail.com>
2024-04-25 11:18:31 +0300

    feat: add loader when loading random card

--- src/pages/learn-cards-page/learn-cards-page.tsx
+++ src/pages/learn-cards-page/learn-cards-page.tsx
@@ -1,31 +1,33 @@
-import { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 
 import { ArrowBackIcon } from '@/assets'
-import { QuestionForm, Typography } from '@/components'
+import { Loader, QuestionForm, Typography } from '@/components'
 import { useGetDeckQuery, useGetRandomCardQuery, useUpdateGradeMutation } from '@/services'
 
 import s from './learn-cards-page.module.scss'
 
 export const LearnCardsPage = () => {
-  const [isFirstLoading, setIsFirstLoading] = useState(true)
   const { deckId = '' } = useParams()
   const { data: deckData } = useGetDeckQuery({ id: deckId })
   const { data: cardData } = useGetRandomCardQuery({ id: deckId })
-  const [updateGrade, { data: randomCardData }] = useUpdateGradeMutation()
+  const [updateGrade, { data: randomCardData, isLoading: isRandomCardLoading }] =
+    useUpdateGradeMutation()
 
-  const card = isFirstLoading ? cardData : randomCardData
+  const card = randomCardData || cardData
   const cardId = randomCardData?.id || cardData?.id || ''
 
   const handleUpdateGrade = (grade: number) => {
     updateGrade({ cardId, grade, id: deckId })
-    setIsFirstLoading(false)
   }
 
   const classNames = {
     content: s.content,
     formWrapper: s.formWrapper,
     linkBack: s.linkBack,
+  }
+
+  if (isRandomCardLoading) {
+    return <Loader />
   }
 
   return (
