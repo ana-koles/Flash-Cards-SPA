90ad4671c3e1e9f0413e16d746ad19a576ec854b
vustinchdev <vustinchdev@gmail.com>
2024-04-29 10:20:31 +0300

    feat: add condition for error display to app-slice

--- src/app-slice.ts
+++ src/app-slice.ts
@@ -3,7 +3,9 @@ import { createSlice, isRejected } from '@reduxjs/toolkit'
 const slice = createSlice({
   extraReducers: builder => {
     builder.addMatcher(isRejected, (state, action: any) => {
-      if (action.payload?.data) {
+      if (action.payload?.data?.errorMessages?.[0]?.message) {
+        state.error = action.payload.data.errorMessages[0].message
+      } else if (action.payload?.data?.message) {
         state.error = action.payload.data.message
       } else {
         state.error = action.payload.error ? action.payload.error : 'Some error occurred'
